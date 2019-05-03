965f8ab4ebd90e2277c04c97bf3f480be8c50a63
John Vandenberg <jayvdb@gmail.com>
2019-05-03 08:32:02 +0700

    Tests and packaging

--- setup.py
+++ setup.py
@@ -11,16 +11,17 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
 Programming Language :: Python :: Implementation :: CPython
 Programming Language :: Python :: Implementation :: PyPy
 Topic :: Software Development :: Quality Assurance
 Topic :: Software Development :: Testing
-Development Status :: 3 - Alpha
+Development Status :: 4 - Beta
 """
 
 setup(
-    name='coverage_env_plugin',
-    version='0.1',
+    name='coverage-env-plugin',
+    version='0.2.0',
     description='coverage.py environment plugin',
     author='John Vandenberg',
     author_email='jayvdb@gmail.com',
@@ -30,6 +31,8 @@ setup(
         'coverage >= 4.0',
         'packaging',
     ],
+    tests_require=['unittest-mixins'],
+    test_suite='tests',
     license='MIT License',
     classifiers=classifiers.splitlines(),
 )
